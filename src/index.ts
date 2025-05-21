9a80c55a1f384e92e00a165e0bcdcc947f4e122c
Cooper Floyd <cooperwfloyd@gmail.com>
2025-05-21 17:39:36 -0500

    use document.documentElement.clientHeight instead of previous calculated solution

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,16 @@
 import type {Vh, VhCalc} from "./types/vhjs";
 
 export const vhCalc: VhCalc = () => {
-  let previousWidth = window.innerWidth;
-  let minAvailHeight: number | null = null;
-
-  const calcMinAvailHeight = (): void => {
-    const innerHeight = window.innerHeight;
-    const screenHeight = window.screen.height;
-
-    if (screenHeight === innerHeight) {
-      minAvailHeight = innerHeight;
-      return;
-    }
-
-    const innerWidth = window.innerWidth;
-    const visualViewportHeight = window?.visualViewport?.height ?? 0;
-    const isMinimumChange =
-      !minAvailHeight || visualViewportHeight < minAvailHeight;
-    const isOrientationChange = previousWidth !== innerWidth;
-
-    if (previousWidth !== innerWidth) previousWidth = innerWidth;
-
-    if (isOrientationChange || isMinimumChange) {
-      minAvailHeight = visualViewportHeight;
-      return;
-    }
-  };
-
   const setVh = (): void => {
     document.documentElement.style.setProperty(
       "--vh-min-avail",
-      `${minAvailHeight ?? 0}px`
+      `${document.documentElement.clientHeight}px`
     );
   };
 
-  calcMinAvailHeight();
   setVh();
-
-  window.addEventListener("resize", () => {
-    calcMinAvailHeight();
-    setVh();
-  });
-
-  window.addEventListener("orientationchange", () => {
-    calcMinAvailHeight();
-    setVh();
-  });
+  window.addEventListener("resize", setVh);
+  window.addEventListener("orientationchange", setVh);
 };
 
 const vh: Vh = () => {
