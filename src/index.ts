b251ba284a8fb8fb8c2bec965841cc1178d22584
Cooper Floyd <cooperwfloyd@gmail.com>
2025-05-21 20:16:53 -0500

    account for dynamic menu bar heights and safari jitters during resize

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
 import type {Vh, VhCalc} from "./types/vhjs";
 
 export const vhCalc: VhCalc = () => {
+  let maxBarHeight: number | null = null;
+
   const setVh = (): void => {
+    const documentClientHeight = document.documentElement.clientHeight;
+    const innerHeight = window.innerHeight;
+    const outerHeight = window.outerHeight;
+    const availHeight = window.screen.availHeight;
+    const viewportHeight = window?.visualViewport?.height ?? 0;
+    const newBarHeight = availHeight - viewportHeight;
+
+    if (maxBarHeight === null || (maxBarHeight && newBarHeight > maxBarHeight))
+      maxBarHeight = newBarHeight;
+
+    const calculatedVh =
+      maxBarHeight &&
+      (outerHeight === viewportHeight || innerHeight !== documentClientHeight)
+        ? availHeight - maxBarHeight
+        : viewportHeight;
+
     document.documentElement.style.setProperty(
       "--vh-min-avail",
-      `${document.documentElement.clientHeight}px`
+      `${calculatedVh}px`
     );
   };
 
   setVh();
-  window.addEventListener("resize", setVh);
+  let resizeTimeout: number | undefined = undefined;
+
+  window.addEventListener("resize", () => {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = window.setTimeout(setVh, 15);
+  });
+
   window.addEventListener("orientationchange", setVh);
 };
 
